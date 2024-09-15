4f10369e1ae00c18e2e02e2376511b465a4e9383
Bhekumuzi Sanele Vilakazi <bvilakazi023a@student.wethinkcode.co.za>
2024-09-15 17:22:35 +0200

    Basic caesar cipher structure done.

---
+++ CaesarCipher.py
@@ -0,0 +1,28 @@
+def encrypt_upper(char):
+    pass
+
+def encrypt_lower(char):
+    pass
+
+def decrypt_upper(char):
+    pass
+
+def decrypt_lower(char):
+    pass
+
+def shift_cipher(text,value):
+    pass
+
+if __name__ == "__main__":
+    print("*** Welcome to Caesar Cipher Progarm ***")
+    
+    while(True):
+        print("\n"+"If you want to end the program enter 1\n")
+
+        word = input("Enter a word to encrypt: ")
+
+        if word == "1":
+            break
+
+    print("*** Thgank You For using the Caesar Cipher Progarm")    
+    
